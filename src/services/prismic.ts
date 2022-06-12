e008bfefe3528e9422339850e176a1883a65c3f1
SkyG0D <silvanosilvino@hotmail.com>
2022-06-12 03:55:11 -0300

    feat: add .env.local and prismic api url

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -2,16 +2,12 @@ import * as prismic from '@prismicio/client';
 import { HttpRequestLike } from '@prismicio/client';
 import { enableAutoPreviews } from '@prismicio/next';
 
-import sm from '../../sm.json';
-
 export interface PrismicConfig {
   req?: HttpRequestLike;
 }
 
-export const endpoint = sm.apiEndpoint;
-
 export function getPrismicClient(config: PrismicConfig = {}): prismic.Client {
-  const client = prismic.createClient(endpoint);
+  const client = prismic.createClient(process.env.PRISMIC_API_URL as string);
 
   enableAutoPreviews({
     client,
